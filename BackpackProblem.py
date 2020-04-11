661a55ac9dc8424bba7c5c2d4f4fe0219805c357
jsneider05 <joan_sneider@hotmail.com>
2020-04-10 19:39:48 -0500

    Backpack problem

--- BackpackProblem.py
+++ BackpackProblem.py
@@ -11,10 +11,25 @@ def morral(tamano, pesos, valores, n):
             morral(tamano, pesos, valores, n - 1))
 
 
+def morral1(tamano, pesos, valores, n):
+  if n == 0 or tamano == 0:
+    return 0
+
+  if pesos[n - 1] > tamano:
+    return morral(tamano, pesos, valores, n - 1)
+
+  agrego = valores[n - 1]
+  tamanoMorralActual = tamano - pesos[n - 1]
+  opcion1Lotoma = agrego + morral(tamanoMorralActual, pesos, valores, n - 1)
+  opcion2noLotoma = morral(tamano, pesos, valores, n - 1)
+  maximo = max(opcion1Lotoma, opcion2noLotoma)
+  return maximo
+
+
 if __name__ == "__main__":
-  valores = [50, 100, 210]
-  pesos = [10, 25, 35]
-  tamano = 35
+  valores = [60, 100, 120]
+  pesos = [10, 20, 30]
+  tamano = 50
   n = len(valores)
   print(f'Valores: {valores}, Pesos: {pesos}')
   resultado = morral(tamano, pesos, valores, n)
